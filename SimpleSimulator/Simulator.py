46534b0fceec1686af38001a5617d5ed81f47e0b
vaibhav25636-svg <vaibhav25636@iiitd.ac.in>
2026-04-10 21:52:55 +0530

    Implemented slt

--- SimpleSimulator/Simulator.py
+++ SimpleSimulator/Simulator.py
@@ -9,4 +9,7 @@ if opcode == "0110011":
         elif funct7 == "0100000": # sub
             registers[rd] = registers[rs1] - registers[rs2]
 
+    elif funct3 == "010": # slt
+        registers[rd] = int(registers[rs1] < registers[rs2])
+
     pc += 4
