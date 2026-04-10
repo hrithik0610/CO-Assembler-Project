e577c853bcb119ce2c2fb817d23e239c8d675f86
Aryan Goyaliya <goyaliyaaryan@gmail.com>
2026-04-10 23:06:29 +0530

    Sign extension

--- SimpleSimulator/Simulator.py
+++ SimpleSimulator/Simulator.py
@@ -6,3 +6,10 @@ import sys
 def read_file(path):
     with open(path, "r") as f:
         return [line.strip() for line in f if line.strip() != ""]
+
+# SIGN EXTENSION
+
+def sign_extend(value, bits):
+    if value & (1 << (bits - 1)):
+        value = value - (1 << bits)
+    return value
