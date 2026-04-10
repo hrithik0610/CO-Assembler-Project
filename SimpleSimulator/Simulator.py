066455d69e4639e291b6464b26deb288496906ae
vaibhav25636-svg <vaibhav25636@iiitd.ac.in>
2026-04-10 21:51:35 +0530

    Implemented add and sub for R type instructions

--- SimpleSimulator/Simulator.py
+++ SimpleSimulator/Simulator.py
@@ -1,14 +1,12 @@
 #Vaibhav's work
-DATA_MEM_BASE = 0x00010000
-DATA_MEM_END  = 0x0001007F
-STACK_BASE    = 0x00000100
-STACK_END     = 0x0000017F
 
-def to_signed32(val):
-    val = val & 0xFFFFFFFF
-    if val >= (1 << 31):
-        val -= (1 << 32)
-    return val
+# R-TYPE
+if opcode == "0110011":
 
-def to_unsigned32(val):
-    return val & 0xFFFFFFFF
+    if funct3 == "000":
+        if funct7 == "0000000": # add
+            registers[rd] = registers[rs1] + registers[rs2]
+        elif funct7 == "0100000": # sub
+            registers[rd] = registers[rs1] - registers[rs2]
+
+    pc += 4
