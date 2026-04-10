a25b9f0d56d5fa40b346879a5100b72a365d9d62
vaibhav25636-svg <vaibhav25636@iiitd.ac.in>
2026-04-10 21:47:18 +0530

    Added 32-bit helper function.

--- SimpleSimulator/Simulator.py
+++ SimpleSimulator/Simulator.py
@@ -3,3 +3,12 @@ DATA_MEM_BASE = 0x00010000
 DATA_MEM_END  = 0x0001007F
 STACK_BASE    = 0x00000100
 STACK_END     = 0x0000017F
+
+def to_signed32(val):
+    val = val & 0xFFFFFFFF
+    if val >= (1 << 31):
+        val -= (1 << 32)
+    return val
+
+def to_unsigned32(val):
+    return val & 0xFFFFFFFF
