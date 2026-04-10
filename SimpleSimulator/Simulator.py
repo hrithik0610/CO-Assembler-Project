d4fce1aec5622a440f0e5a284fe10c63dee69206
Mayank Sharma <140956050+mxyxnk518@users.noreply.github.com>
2026-04-10 23:07:17 +0530

    added print state and print memory
    added print state and print memory

--- SimpleSimulator/Simulator.py
+++ SimpleSimulator/Simulator.py
@@ -13,3 +13,21 @@ def sign_extend(value, bits):
     if value & (1 << (bits - 1)):
         value = value - (1 << bits)
     return value
+
+# PRINT STATE
+
+def print_state(pc, registers, outfile):
+    line = format(pc, '032b')
+    for reg in registers:
+        line += " " + format(reg & 0xFFFFFFFF, '032b')
+    outfile.write(line + "\n")
+
+
+# PRINT MEMORY
+
+def print_memory(memory, outfile):
+    base = 0x00010000
+    for i in range(32):
+        addr = base + i * 4
+        val = format(memory[i] & 0xFFFFFFFF, '032b')
+        outfile.write(f"{hex(addr)}: {val}\n")
